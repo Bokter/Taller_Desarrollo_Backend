97ddddb7ea2194c272051ac853e6231258185585
Canela <carpinteron@uninorte.edu.co>
2026-02-27 10:51:52 -0500

    get urls with n

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 //const express = require('express');
 //const app = express();
 
-async function obtenerMonsters1() {
-  const response = await fetch(
-    `https://www.dnd5eapi.co/api/2014/monsters`
-  );
 
-  const data = await response.json();
-  return data.results; // [{ name, url }]
-
-}
-
-async function obtenerMonsters2(limit = 1) {
+async function obtenerMonsters(limit = 2) {
   const response = await fetch(
     `https://www.dnd5eapi.co/api/2014/monsters`
   );
   
   const data = await response.json();
-  const data2=data.results.slice(0, limit);
-  return data2; // [{ name, url }]
-
+  const results=data.results.slice(0, limit);
+  const urls=results.reduce((acum,m)=>{
+    acum.push(m.url);
+    return acum;
+  },[])
+  return urls; // [{ name, url }]
 }
-let monsters= await obtenerMonsters2();
-console.log(monsters);
+let monsters= await obtenerMonsters();
+console.log( monsters);
