4b8c280651ea78a76fdcd3bddc6514cc97c4900c
Bokter <rojasdjuan18@gmail.com>
2026-02-27 11:50:22 -0500

    Creating queries

--- index.js
+++ index.js
@@ -11,5 +11,52 @@ async function obtenerMonsters(limit = 20) {
 
 }
 
-let monsters= await obtenerMonsters();
-console.log(monsters);
+// a
+
+const resultFilter = monsters.filter((monster) => 
+  monster.cr >= 15 && monster.hp >= 200
+);
+
+console.log(resultFilter);
+
+// b
+
+const resultFind = monsters.find((monster) =>
+  monster.type === "dragon" && monster.cr >= 6
+);
+
+console.log(resultFind)
+
+// find: primer monstruo que sea type === "dragon" y cr >= 6
+
+// c
+
+const isLegendary = (monster) => monster.HasLegendary = true;
+
+console.log(monsters.some(isLegendary));
+
+// d
+
+console.log(monsters.every((monster) =>
+  monster.hp > 0 && Object.keys(monster.stats).length === 6
+));
+
+// e
+
+const result = monsters.reduce((accum, monster) => {
+  const type = monster.type;
+
+  if (!accum[type]) {
+    accum[type] = { count: 0, totalCR: 0, avgCR: 0, maxHP: 0 };
+  }
+
+  accum[type].count += 1;
+  accum[type].totalCR += monster.cr;
+  accum[type].avgCR = Math.round((accum[type].totalCR / accum[type].count) * 10) / 10;
+  accum[type].maxHP = Math.max(accum[type].maxHP, monster.hp);
+
+  return accum;
+}, {});
+
+console.log(result)
+// every: ¿todos tienen stats completos (6 atributos) y hp > 0?
