0ca9c3d284237f96515cfce9eed606a48d6cb18c
Bokter <rojasdjuan18@gmail.com>
2026-02-27 11:58:27 -0500

    Done

--- index.js
+++ index.js
@@ -22,10 +22,47 @@ async function obtenerMonsters(limit = 2) {
   const responses = await Promise.all(promises)
   const d = await Promise.all(responses.map(async r => r.json()));
 
-  return d; // [{ name, url }]
+  let monsters = d.reduce((accum, m) => {
+
+    const max_speed = Object.values(m.speed).reduce((max_speed, s) => {
+      const new_speed = parseFloat(s.split(" ")[0]);
+      max_speed = new_speed > max_speed ? new_speed:max_speed;
+      return max_speed;
+    }, 0.0);
+
+    accum.push(
+      {
+        index: m.index,
+        name: m.name,
+        size: m.size,
+        type: m.type,
+        alignment: m.alignment,
+        cr: m.challenge_rating,
+        ac: m.armor_class,
+        hp: m.hit_points,
+        speed: max_speed,
+        stats: {
+          str:m.strength,
+          dex:m.dexterity,
+          con:m.constitution,
+          int:m.intelligence,
+          wis:m.wisdom,
+          cha:m.charisma
+        },
+        immuneCount: m.damage_immunities.length,
+        resistCount: m.damage_resistances.length,
+        vulCount: m.damage_vulnerabilities.length,
+        hasLegendary: m.legendary_actions.length > 0 ? true:false
+      }
+    );
+    return accum
+    }, []);
+
+  return monsters; // [{ name, url }]
 
 }
 
+let monsters = await obtenerMonsters(10);
 // a
 
 const resultFilter = monsters.filter((monster) => 
@@ -94,41 +131,3 @@ const resultBucket  = monsters.reduce((accum, monster) => {
 console.log(resultBucket);
 let response = await obtenerMonsters();
 
-let monsters = response.reduce((accum, m) => {
-
-  const max_speed = Object.values(m.speed).reduce((max_speed, s) => {
-    const new_speed = parseFloat(s.split(" ")[0]);
-    max_speed = new_speed > max_speed ? new_speed:max_speed;
-    return max_speed;
-  }, 0.0);
-
-  accum.push(
-    {
-      index: m.index,
-      name: m.name,
-      size: m.size,
-      type: m.type,
-      alignment: m.alignment,
-      cr: m.challenge_rating,
-      ac: m.armor_class,
-      hp: m.hit_points,
-      speed: max_speed,
-      stats: {
-        str:m.strength,
-        dex:m.dexterity,
-        con:m.constitution,
-        int:m.intelligence,
-        wis:m.wisdom,
-        cha:m.charisma
-      },
-      immuneCount: m.damage_immunities.length,
-      resistCount: m.damage_resistances.length,
-      vulCount: m.damage_vulnerabilities.length,
-      hasLegendary: m.legendary_actions.length > 0 ? true:false
-    }
-  );
-  
-  return accum
-}, []);
-
-console.log( monsters);
