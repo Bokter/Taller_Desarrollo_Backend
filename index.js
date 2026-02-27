131ac5fa3c13ea71ceddd1e6befbaee777e80bd6
SAMUEL ISAAC CAMARGO CONSUEGRA <sicamargo@uninorte.edu.co>
2026-02-27 11:47:18 -0500

    monsters properly formatted

--- index.js
+++ index.js
@@ -25,5 +25,44 @@ async function obtenerMonsters(limit = 2) {
   return d; // [{ name, url }]
 
 }
-let monsters= await obtenerMonsters();
+
+let response = await obtenerMonsters();
+
+let monsters = response.reduce((accum, m) => {
+
+  const max_speed = Object.values(m.speed).reduce((max_speed, s) => {
+    const new_speed = parseFloat(s.split(" ")[0]);
+    max_speed = new_speed > max_speed ? new_speed:max_speed;
+    return max_speed;
+  }, 0.0);
+
+  accum.push(
+    {
+      index: m.index,
+      name: m.name,
+      size: m.size,
+      type: m.type,
+      alignment: m.alignment,
+      cr: m.challenge_rating,
+      ac: m.armor_class,
+      hp: m.hit_points,
+      speed: max_speed,
+      stats: {
+        str:m.strength,
+        dex:m.dexterity,
+        con:m.constitution,
+        int:m.intelligence,
+        wis:m.wisdom,
+        cha:m.charisma
+      },
+      immuneCount: m.damage_immunities.length,
+      resistCount: m.damage_resistances.length,
+      vulCount: m.damage_vulnerabilities.length,
+      hasLegendary: m.legendary_actions.length > 0 ? true:false
+    }
+  );
+  
+  return accum
+}, []);
+
 console.log( monsters);
